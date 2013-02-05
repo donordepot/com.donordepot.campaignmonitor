85a4ef9f680c0973b8f18e5329dd40c45a82ec57
David Barratt <dbarratt@donordepot.com>
2013-02-05 14:58:43 -0500

    Change form to be a setting form instead of a config form.

--- CRM/Campaignmonitor/Form/Config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'CRM/Core/Form.php';
-
-class CRM_Campaignmonitor_Form_Config extends CRM_Core_Page {
-  
-}
---
+++ CRM/Campaignmonitor/Form/Setting.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'CRM/Core/Form.php';
+
+class CRM_Campaignmonitor_Form_Setting extends CRM_Core_Page {
+  
+}
