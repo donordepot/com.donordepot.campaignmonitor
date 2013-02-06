c7ba9d78a967f8b6fa627fc43775767a33302d32
David Barratt <dbarratt@donordepot.com>
2013-02-06 13:18:53 -0500

    Save the Groups in the database and begin working the the Campaign Monitor API.

--- CRM/Campaignmonitor/Form/Setting.php
+++ CRM/Campaignmonitor/Form/Setting.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'CRM/Core/Form.php';
+require_once 'packages/Net/Curl.php';
 
 class CRM_Campaignmonitor_Form_Setting extends CRM_Core_Form {
   
@@ -29,7 +29,24 @@ class CRM_Campaignmonitor_Form_Setting extends CRM_Core_Form {
     $defaults['api_key'] = CRM_Core_BAO_Setting::getItem('Campaign Monitor Preferences',
       'api_key', NULL, FALSE
     );
-
+    
+    $groups = CRM_Contact_BAO_Group::getGroups();
+    
+    $checkboxes = array();
+    foreach ($groups as $group) {
+      $checkboxes[] = &HTML_QuickForm::createElement('checkbox', $group->id, $group->name);
+    }
+    $this->addGroup($checkboxes, 'groups', ts('Groups'));
+    
+    $current = CRM_Core_BAO_Setting::getItem('Campaign Monitor Preferences',
+      'groups', NULL, FALSE
+    );
+    
+    if (!empty($current)) {
+      foreach ($current as $key => $value) {
+        $defaults['groups'][$key] = $value;
+      }
+    }
 
     $buttons = array(
       array(
@@ -53,15 +70,66 @@ class CRM_Campaignmonitor_Form_Setting extends CRM_Core_Form {
    * @return None
    */
   public function postProcess() {
+  
     // store the submitted values in an array
     $params = $this->controller->exportValues($this->_name);
 
-    // save checksum timeout
     if (CRM_Utils_Array::value('api_key', $params)) {
+      // Save the api_key.
       CRM_Core_BAO_Setting::setItem($params['api_key'],
         'Campaign Monitor Preferences',
         'api_key'
       );
+    }
+    
+    if (CRM_Utils_Array::value('groups', $params)) {
+      CRM_Core_BAO_Setting::setItem($params['groups'],
+        'Campaign Monitor Preferences',
+        'groups'
+      );
+    }
+    
+    if (CRM_Utils_Array::value('api_key', $params) && CRM_Utils_Array::value('groups', $params)) {
+    
+      // Update the Group Map.
+      $group_map = CRM_Core_BAO_Setting::getItem('Campaign Monitor Preferences',
+        'group_map', NULL, FALSE
+      );
+      
+      $group_map = !empty($group_map) ? $group_map : array();
+      
+      $groups = CRM_Contact_BAO_Group::getGroups();
+      
+      $group_ids = array();
+      foreach ($groups as $group) {
+        $group_ids[$group->id] = $group->id;
+      }
+      
+      // print '<pre>'.print_r($groups, TRUE).'</pre>';
+      
+      // If any Groups have been removed, remove the map.
+      foreach ($group_map as $key => $value) {
+        if (!in_array($key, $group_ids)) {
+          unset($group_map[$key]);
+        }
+      }
+      
+      // For each Group, make sure it is mapped
+      foreach ($groups as $group) {
+        
+        // Only Map Groups that are upposed to be synced.
+        if (empty($params['groups'][$group->id])) {
+          continue;
+        }
+        
+        $curl = new Net_Curl();
+        $curl->url = 'https://graph.facebook.com/davidbarratt';
+        $result = $curl->execute();
+        
+        // print '<pre>'.print_r($result, TRUE).'</pre>';
+        
+      }
+    
     }
     
   }
--- templates/CRM/Campaignmonitor/Form/Setting.tpl
+++ templates/CRM/Campaignmonitor/Form/Setting.tpl
@@ -1,7 +1,9 @@
 <div class="crm-block crm-form-block crm-campaignmonitor-setting-form-block">
-  <div id="help">
+  <!--
+  <div class="crm-error">
     {ts}You'll need to get an <a href="http://www.campaignmonitor.com/api/getting-started#apikey">API Key</a> from Campaign Monitor for these settings.{/ts} 
   </div>
+  -->
   <div class="crm-block crm-form-block crm-campaignmonitor-setting-form-block">
     <fieldset class="form-wrapper">
       <legend><span>{ts}API Key{/ts}</span></legend>
@@ -17,6 +19,23 @@
             {ts}<a href="http://www.campaignmonitor.com/api/getting-started#apikey">API Key</a> from Campaign Monitor{/ts}
           </div>
         </div>
+      </div>
+    </fieldset>
+    <fieldset class="form-wrapper">
+      <legend><span>Groups</span></legend>
+      <div class="fieldset-wrapper">
+        <div class="description">
+          {ts}Groups that should by Synced with Campaign Monitor Lists.{/ts}
+        </div>
+        {foreach from=$form.groups item=group}
+          {if $group|is_array}
+            <div class="form-item form-type-checkbox clearfix">
+              <div class="input-label">
+                {$group.html} {$group.label}
+              </div>
+            </div>
+          {/if}
+        {/foreach}
       </div>
     </fieldset>
     <div class="crm-submit-buttons">
