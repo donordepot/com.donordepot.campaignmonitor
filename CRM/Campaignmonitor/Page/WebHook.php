a6cce38fff33c0498e9dc98d671b218d809730d5
David Barratt <dbarratt@donordepot.com>
2013-02-07 12:12:24 -0500

    Prepare the webhook for a standard JSON response.

--- CRM/Campaignmonitor/Page/WebHook.php
+++ CRM/Campaignmonitor/Page/WebHook.php
@@ -1,15 +1,20 @@
 <?php
 
-require_once 'CRM/Core/Page.php';
-
 class CRM_Campaignmonitor_Page_WebHook extends CRM_Core_Page {
+
   function run() {
-    // Example: Set the page-title dynamically; alternatively, declare a static title in xml/Menu/*.xml
-    CRM_Utils_System::setTitle(ts('WebHook'));
-
-    // Example: Assign a variable for use in a template
-    $this->assign('currentTime', date('Y-m-d H:i:s'));
-
-    parent::run();
+    
+    $data = array(
+      'hello' => 'world!',
+    );
+    
+    // Return the JSON output
+    header('Content-type: application/json');
+    $json = new Services_JSON();
+    $return = $json->encode($data);
+    print $return;
+    CRM_Utils_System::civiExit();
+    
   }
+  
 }
